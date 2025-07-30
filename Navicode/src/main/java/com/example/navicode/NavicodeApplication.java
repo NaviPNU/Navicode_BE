d0e274210b156263add7c9f942fa0bf3186076f3
hyeeun <silver807@pusan.ac.kr>
2025-07-30 12:29:30 +0900

    navicode 개발환경 설정

---
+++ Navicode/src/main/java/com/example/navicode/NavicodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.navicode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NavicodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NavicodeApplication.class, args);
+	}
+
+}
---
+++ Navicode/src/test/java/com/example/navicode/NavicodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.navicode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NavicodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
